9319675142af2d54c1bf3f6e466675f8d8bbd667
Wojciech Malarski <w.malarski@gmail.com>
2024-06-11 21:48:06 +0200

    vitest added

--- apps/docs/stories/button.stories.tsx
+++ apps/docs/stories/button.stories.tsx
@@ -30,11 +30,6 @@ export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/react/api/csf
- * to learn how to use render functions.
- */
 export const Primary: Story = {
 	render: (props) => (
 		<Button
