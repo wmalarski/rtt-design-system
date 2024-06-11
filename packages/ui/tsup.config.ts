f8fe3037b980d6521cb2c3734510bc5e7febb9f9
Wojciech Malarski <w.malarski@gmail.com>
2024-06-11 20:33:39 +0200

    adds new options to tsup config

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-	entryPoints: ["src/button.tsx", "src/styles.css"],
+	entryPoints: ["src/button/button.tsx", "src/styles.css"],
 	format: ["cjs", "esm"],
 	dts: true,
 	sourcemap: true,
 	external: ["react"],
+	treeshake: true,
+	splitting: true,
+	minify: true,
+	clean: true,
 	...options,
 }));
